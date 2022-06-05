580f57ac4049ebd14d132839c4b758a1d84edc20
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2022-06-05 16:59:05 +0530

    Create Day2.cpp

---
+++ Day2.cpp
@@ -0,0 +1,7 @@
+double findNthRootOfM(int n, long long m) {
+	// Write your code here.
+    double t=1.000000/n;
+    double ans=pow(m,t);
+    return ans;
+}
+//question: https://www.codingninjas.com/codestudio/problems/nth-root-of-m_1062679?topList=striver-sde-sheet-problems&leftPanelTab=1
